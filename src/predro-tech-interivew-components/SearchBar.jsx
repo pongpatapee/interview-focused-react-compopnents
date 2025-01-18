920405978678719a957cdea3962b2bf653ada507
pongpatapee <pongpatapee1021@gmail.com>
2025-01-18 17:48:41 +0700

    improvements: include instead of startsWith, and bolding search term

--- src/predro-tech-interivew-components/SearchBar.jsx
+++ src/predro-tech-interivew-components/SearchBar.jsx
@@ -17,12 +17,31 @@ const SearchBar = () => {
   ];
 
   const [searchFruits, setSearchFruit] = useState([...fruits]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleOnChange = (event) => {
     setSearchFruit(
       fruits.filter((fruit) =>
-        fruit.toLowerCase().startsWith(event.target.value.toLowerCase()),
+        fruit.toLowerCase().includes(event.target.value.toLowerCase()),
       ),
+    );
+
+    setSearchTerm(event.target.value);
+  };
+
+  const boldSearchTerm = (fruit) => {
+    let includeIdx = fruit.toLowerCase().indexOf(searchTerm.toLowerCase());
+
+    let start = fruit.slice(0, includeIdx);
+    let middle = fruit.slice(includeIdx, includeIdx + searchTerm.length);
+    let end = fruit.slice(includeIdx + searchTerm.length, fruit.length);
+
+    return (
+      <span>
+        {start}
+        <b>{middle}</b>
+        {end}
+      </span>
     );
   };
 
@@ -34,11 +53,15 @@ const SearchBar = () => {
         onChange={handleOnChange}
       />
 
-      <ul>
-        {searchFruits.map((fruit) => (
-          <li>{fruit}</li>
-        ))}
-      </ul>
+      {searchFruits && searchFruits.length ? (
+        <ul>
+          {searchFruits.map((fruit) => (
+            <li>{boldSearchTerm(fruit)}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Your search term does not exist</p>
+      )}
     </div>
   );
 };
